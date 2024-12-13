6f1b3583115f7d35ccc2d0b62039a32240bd4fb9
Xavier Perez <66711995+LOLCR4FT@users.noreply.github.com>
2024-12-13 10:44:35 +0100

    Add front

---
+++ index.js
@@ -0,0 +1,15 @@
+const vacations = [
+  ['18/12/2024', '29/12/2024'],
+  ['01/01/2025', '07/01/2024']
+];
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('a')
+
+  
+  
+
+
+
+});
